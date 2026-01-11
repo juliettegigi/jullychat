3a843f2b1f77f49a2ae0d4afa34edab4ac577ab1
juliettegigi <mar9ina@gmail.com>
2026-01-11 17:25:18 -0300

    environment pack json

--- my-app/src/app/core/services/api-user.service.ts
+++ my-app/src/app/core/services/api-user.service.ts
@@ -2,7 +2,7 @@ import { Injectable,inject } from '@angular/core';
 import { HttpClient ,HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { environment } from '../../../environments/environment.prod'
+import { environment } from '../../../environments/environment'
 import { GetUserByIdResponse, rtaLogin, User } from '../models/user';
 //import { getApiUrl } from '../config/api.config';
 
